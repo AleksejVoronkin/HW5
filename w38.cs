c256ab64a743b5be81c18a7c3102ddbb4bca4d2b
AleksejVoronkin <120055748+AleksejVoronkin@users.noreply.github.com>
2023-01-18 22:43:47 +0300

    Add files via upload

---
+++ w38.cs
@@ -0,0 +1,37 @@
+﻿// Задача 38: Задайте массив чисел. Найдите разницу между максимальным и минимальным элементов массива.
+//[3 7 22 2 78] -> 76
+
+
+void input_array(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(1, 100);
+}
+
+int release(int[] array)
+{
+int result = 0;
+int min = 0;
+int max = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[min] > array[i])
+        min = i;
+    else
+    if (array[max] < array[i])
+        max = i;
+    }
+if (array[min] > array[max])
+    result = array[min] - array[max];
+else if (array[max] > array[min])
+    result = array[max] - array[min];
+return result;
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+input_array(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine(" Разницу между максимальным и минимальным элементами массива = "+release(array));
